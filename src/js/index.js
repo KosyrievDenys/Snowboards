1336f92caddf3bad54d2259f2aa351490739e007
denys_kosyriev <utross2241@gmail.com>
2022-11-10 01:20:03 +0200

    3

--- src/js/index.js
+++ src/js/index.js
@@ -12,10 +12,10 @@
 
 const headerBurger = document.querySelector('.header-burger');
 const headerNav = document.querySelector('.header-nav');
-    headerBurger.addEventListener('click', function (e){
-        headerBurger.classList.toggle('active-header-burger');
-        headerNav.classList.toggle('active-header-nav');
-    })
+headerBurger.addEventListener('click', function (e) {
+  headerBurger.classList.toggle('active-header-burger');
+  headerNav.classList.toggle('active-header-nav');
+})
 
 // const swiper = new Swiper('.intro-slider', {
 //     loop: true,
@@ -28,34 +28,35 @@ const headerNav = document.querySelector('.header-nav');
 // });
 
 new Swiper('.intro-swiper', {
-    loop: true,
-    autoplay: {
-        delay: 4000,
-    },
-    simulateTouch: false,
-    speed: 800,
-    watchOverflow: true,
+  loop: true,
+  // autoplay: {
+  //   // delay: 4000,
+  // },
+  simulateTouch: false,
+  speed: 800,
+  watchOverflow: true,
 });
-//
-// new Swiper('.series-swiper', {
-//     // navigation: {
-//     //     nextEl: '.swiper-button-next',
-//     //     prevEl: '.swiper-button-prev',
-//     // },
-//     slidesPerView: 3,
-//     // slidesPerGroup: 1,
-//     // spaceBetween: 30,
-//     // keyboard: {
-//     //     enabled: true,
-//     //     onlyInViewport: true,
-//     //     pageUpDown: true,
-//     // },
-//     // mousewheel: {
-//     //     sensitivity: 1,
-//     //     eventsTarget: '.series-slider',
-//     // },
-//     // watchOverflow: true,
-//     // initialSlide: 3,
-//     // speed: 800,
-// });
+
+new Swiper('.series-swiper', {
+  slidesPerView: 3,
+  centeredSlides: true,
+  initialSlide: 1,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // slidesPerGroup: 1,
+  // spaceBetween: 30,
+  // keyboard: {
+  //     enabled: true,
+  //     onlyInViewport: true,
+  //     pageUpDown: true,
+  // },
+  // mousewheel: {
+  //     sensitivity: 1,
+  //     eventsTarget: '.series-slider',
+  // },
+  // watchOverflow: true,
+  // speed: 800,
+});
 
