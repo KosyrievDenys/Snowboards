488205e680b5ca780e03f8c96f7e42b85cdc7ccb
denys_kosyriev <utross2241@gmail.com>
2022-11-09 14:34:12 +0200

    2

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,23 @@
-// new Swiper('.intro-slider', {
+'use strict'
+// import Swiper,{Navigation} from 'swiper/bundle';
+//
+// Swiper.use([Navigation]);
+
+// const headerBurger = document.querySelector('.header-burger')
+// headerBurger.onclick = () => {
+//     const headerNav = document.querySelector('.header-nav');
+//     headerBurger.classList.toggle('action-header-burger');
+//     headerNav.classList.toggle('action-header-nav');
+// }
+
+const headerBurger = document.querySelector('.header-burger');
+const headerNav = document.querySelector('.header-nav');
+    headerBurger.addEventListener('click', function (e){
+        headerBurger.classList.toggle('active-header-burger');
+        headerNav.classList.toggle('active-header-nav');
+    })
+
+// const swiper = new Swiper('.intro-slider', {
 //     loop: true,
 //     autoplay: {
 //         delay: 4000,
@@ -8,24 +27,35 @@
 // watchOverflow: true,
 // });
 
-// new Swiper('.series-slider', {
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-//     slidesPerView: 1,
-//     slidesPerGroup: 1,
-//     keyboard: {
-//         enabled: true,
-//         onlyInViewport: true,
-//         pageUpDown: true,
-//     },
+new Swiper('.intro-swiper', {
+    loop: true,
+    autoplay: {
+        delay: 4000,
+    },
+    simulateTouch: false,
+    speed: 800,
+    watchOverflow: true,
+});
+//
+// new Swiper('.series-swiper', {
+//     // navigation: {
+//     //     nextEl: '.swiper-button-next',
+//     //     prevEl: '.swiper-button-prev',
+//     // },
+//     slidesPerView: 3,
+//     // slidesPerGroup: 1,
+//     // spaceBetween: 30,
+//     // keyboard: {
+//     //     enabled: true,
+//     //     onlyInViewport: true,
+//     //     pageUpDown: true,
+//     // },
 //     // mousewheel: {
 //     //     sensitivity: 1,
 //     //     eventsTarget: '.series-slider',
 //     // },
-//     watchOverflow: true,
-//     initialSlide: 1,
-//     speed: 800,
+//     // watchOverflow: true,
+//     // initialSlide: 3,
+//     // speed: 800,
 // });
 
